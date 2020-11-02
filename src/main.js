f2e6840d9eec2f419cb7e50d13c354c1f28b90a1
tcdw <tcdw2011@gmail.com>
2020-11-03 00:34:21 +0800

    样式完善继续

--- src/main.js
+++ src/main.js
@@ -1,3 +1,19 @@
 import './scss/main.scss';
 
-console.log('hmmmm');
+const menuDesktop = document.getElementById('menu-dt');
+const bannerHeight = 11;
+
+const navTopStyle = () => {
+    const scroll = window.pageYOffset;
+    const fontSize = Number.parseInt(
+        window.getComputedStyle(document.documentElement).fontSize, 10,
+    );
+    if (scroll > bannerHeight * fontSize) {
+        menuDesktop.classList.remove('top');
+    } else {
+        menuDesktop.classList.add('top');
+    }
+};
+
+window.addEventListener('scroll', navTopStyle, false);
+navTopStyle();
