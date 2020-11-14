78277f4726773c6e86113ad75ca5484e13cf382f
tcdw <tcdw2011@gmail.com>
2020-11-15 00:58:59 +0800

    高度适应实现

--- src/comment-mockup.js
+++ src/comment-mockup.js
@@ -1,4 +1,8 @@
 if (document.body.classList.contains('post')) {
     const actualContent = document.getElementById('actual-content');
     const ghostContent = document.getElementById('ghost-content');
+    actualContent.addEventListener('input', () => {
+        ghostContent.textContent = `\u200b${actualContent.value}\u200b`;
+        actualContent.style.height = `${ghostContent.getBoundingClientRect().height}px`;
+    });
 }
