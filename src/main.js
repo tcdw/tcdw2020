4a572268359b635a8c45bf89bde72dae1bffab12
tcdw <tcdw2011@gmail.com>
2020-12-20 18:40:30 +0800

    for old silverblog

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import './scss/main.scss';
-import './comment-mockup';
+// import './comment-mockup';
 
 const desktopMenu = document.getElementById('menu-dt');
 const mobileMenu = document.getElementById('menu-sm');
